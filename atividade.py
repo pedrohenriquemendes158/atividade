8a22c04ce1996bad704377af77adb392ee0a048a
unknown <henriquemendesp85@gmail.com>
2023-08-11 19:52:53 -0300

    first commit

---
+++ atividade.py
@@ -0,0 +1,6 @@
+nome = input ("digite seu nome:")
+sobrenome = (input ("digite seu sobrenome:"))
+idade = int(input ("digite sua idade:"))
+peso = int (input ("digite seu peso:"))
+
+print (nome,sobrenome,idade,peso)
