583d2bc74cff24dc2968c07fe107f5847f2f3941
unknown <henriquemendesp85@gmail.com>
2023-08-11 20:34:08 -0300

    hdfhed

---
+++ atividade03.py
@@ -0,0 +1,15 @@
+# Armazenando informações nas variáveis
+nome = "SeuNome"
+sobrenome = "SeuSobrenome"
+idade = 25
+altura = 1.75
+peso = 70.5
+maior_de_idade = idade >= 18
+
+# Exibindo os resultados
+print("Nome:", nome)
+print("Sobrenome:", sobrenome)
+print("Idade:", idade, "anos")
+print("Altura:", altura, "metros")
+print("Peso:", peso, "quilos")
+print("Maior de idade:", maior_de_idade)
